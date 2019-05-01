1a6ed14f2bf70b6a745f7d2afa269204d6bd33bc
Davtian001 <davit.davtyan.00@mail.ru>
2019-05-02 00:33:43 +0500

    not-found 404+

--- shop/src/app/app-routing.module.ts
+++ shop/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WorkingWithDbComponent } from './root-components/working-with-db/working-with-db.component'
+import { NotFoundComponent } from './root-components/not-found/not-found.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'db', component: WorkingWithDbComponent},
+  {path: 'aa', component: NotFoundComponent},
 ];
 
 @NgModule({
