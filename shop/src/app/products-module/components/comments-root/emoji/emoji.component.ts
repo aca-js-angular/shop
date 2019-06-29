67147ac99a9b9b7ae5467437570b0725da4bb279
Davtian001 <davit.davtyan.00@mail.ru>
2019-06-29 23:50:37 +0400

    product comment add emoji translateX animate

--- shop/src/app/products-module/components/comments-root/emoji/emoji.component.ts
+++ shop/src/app/products-module/components/comments-root/emoji/emoji.component.ts
@@ -4,12 +4,13 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-emoji',
   templateUrl: './emoji.component.html',
-  styleUrls: ['./emoji.component.scss']
+  styleUrls: ['./emoji.component.scss'],
+  inputs: ['animate']
 })
 export class EmojiComponent implements OnInit {
 
   @Output('setEmoji') setEmoji = new EventEmitter<string>();
-
+  animate: boolean;
   emojis: string[];
 
 
